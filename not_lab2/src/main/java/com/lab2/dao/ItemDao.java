9a4a1c5fb1b7f387b7016604ccffb4982279936f
raser891@student.liu.se <raser891@student.liu.se>
2016-05-01 11:48:17 +0200

    removing old files

--- not_lab2/src/main/java/com/lab2/dao/ItemDao.java
+++
@@ -1,9 +0,0 @@
-package com.lab2.dao;
-
-import com.lab2.entity.Item;
-
-/**
- * Created by rasmus on 4/21/16.
- */
-public interface ItemDao extends BaseDao<Item,Item>{
-}
--- not_lab2/src/main/java/com/lab2/dao/impl/PartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lab2.dao.impl;
-
-/**
- * Created by rasmus on 4/21/16.
- */
-public class PartRepository {
-}
